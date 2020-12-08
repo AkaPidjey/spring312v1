6dafc05970ccff60842f4f9a535cdee276d95c98
AkaPidjey <pidjey@gmail.com>
2020-12-08 20:18:50 +0300

    Initial commit

---
+++ src/main/java/web/spring312v1/DAO/UserDao.java
@@ -0,0 +1,18 @@
+package web.spring312v1.DAO;
+
+import web.spring312v1.model.Role;
+import web.spring312v1.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserDao {
+
+    void createNewUser(User user);
+    void editUser(User user);
+    void deleteUserById(Long id);
+    Optional<User> getUserById(Long id);
+    List<User> getAllUsers();
+    Optional<User> getUserByLogin(String login);
+    Optional<Role> getRoleByName(String name);
+}
---
+++ src/main/java/web/spring312v1/controller/UserControllerNew.java
@@ -0,0 +1,59 @@
+package web.spring312v1.controller;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import web.spring312v1.model.Role;
+import web.spring312v1.model.User;
+import web.spring312v1.service.UserService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Controller
+public class UserControllerNew {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/new_user")
+    public String newUser() {
+        return "new_user";
+    }
+
+    @PostMapping("/new_user")
+    public String createNewUser(@ModelAttribute("user") User user, @RequestParam("role") String role) {
+        user.setPasswordReal(user.getPassword());
+        Set<Role> roleSet = new HashSet<>();
+        if (role.equals("ROLE_ADMIN")){
+            roleSet.add(userService.getRoleByName("ROLE_ADMIN").get());
+            roleSet.add(userService.getRoleByName("ROLE_USER").get());
+        } else {
+            roleSet.add(userService.getRoleByName("ROLE_USER").get());
+        }
+        user.setRoles(roleSet);
+        userService.createNewUser(user);
+        return "redirect:/new_user";
+    }
+
+    @GetMapping("/user")
+    public ModelAndView showUser() {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("user_page");
+        modelAndView.addObject("user", user);
+        return modelAndView;
+    }
+
+}
